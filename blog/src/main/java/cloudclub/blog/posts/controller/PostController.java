932c1b54e49c5fece4fd6abae1d6295117c9bf6c
seoyeon0201 <psy0201123@gmail.com>
2024-05-30 19:10:03 +0900

    feat: 예외처리 추가

--- blog/src/main/java/cloudclub/blog/posts/controller/PostController.java
+++ blog/src/main/java/cloudclub/blog/posts/controller/PostController.java
@@ -1,9 +1,10 @@
 package cloudclub.blog.posts.controller;
 
 import cloudclub.blog.posts.dto.PostRequestsDto;
-import cloudclub.blog.posts.service.PostHashtagService;
+import cloudclub.blog.posts.config.ResultMessage;
 import cloudclub.blog.posts.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class PostHashtagController {
+public class PostController {
 
     private final PostService postService;
 
-    @PostMapping("/post")
-    public Long updatePost(@RequestBody PostRequestsDto requestsDto, @RequestHeader Long userId) throws Exception {
+    @PostMapping("/v1/post")
+    public ResponseEntity<ResultMessage> updatePost(@RequestBody PostRequestsDto requestsDto, @RequestHeader Long userId) throws Exception {
         return postService.save(requestsDto, userId);
     }
 }
--- blog/src/main/java/cloudclub/blog/posts/service/PostService.java
+++ blog/src/main/java/cloudclub/blog/posts/service/PostService.java
@@ -1,14 +1,18 @@
 package cloudclub.blog.posts.service;
 
+import cloudclub.blog.posts.config.CustomException;
+import cloudclub.blog.posts.config.StatusEnum;
 import cloudclub.blog.posts.dto.PostRequestsDto;
+import cloudclub.blog.posts.config.ResultMessage;
 import cloudclub.blog.posts.entity.Post;
 import cloudclub.blog.posts.repository.PostRepository;
 import cloudclub.blog.posts.repository.HashtagRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Service
 @Transactional
@@ -19,16 +23,15 @@ public class PostService {
     private final HashtagRepository hashtagRepository;
 
     //게시글 등록
-    public Long save(PostRequestsDto postRequestsDto, Long userId) throws Exception {
+    public ResponseEntity<ResultMessage> save(PostRequestsDto postRequestsDto, Long userId) throws Exception {
         //예외처리
 
         if (postRequestsDto.title() == null) {
-            throw new Exception("Title cannot be null");
+            throw new CustomException("Title cannot be null", "411");
         }
         if (postRequestsDto.contents() == null) {
-            throw new Exception("Content cannot be null");
+            throw new CustomException("Content cannot be null", "412");
         }
-
 
         //게시글 저장
         Post post = postRepository.save(
@@ -41,7 +44,16 @@ public class PostService {
         //해시태그 저장
         postHashtagService.saveHashtag(post, postRequestsDto.tagNames());
 
-        return post.getId();
+        //response
+        ResultMessage message = new ResultMessage();
+        HttpHeaders headers = new HttpHeaders();
+
+        message.setStatus(StatusEnum.OK);
+        message.setData(postRequestsDto);
+        message.setMessage("Post created");
+        headers.set("userId", String.valueOf(userId));
+
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 
 }
