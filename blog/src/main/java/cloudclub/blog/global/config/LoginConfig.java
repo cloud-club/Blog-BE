bd25aae55cbdb2b3544c8fdcddacab16a7798863
mirageoasis <kimhw0820@naver.com>
2024-06-09 00:29:42 +0900

    feat: 로그인 어노테이션 추가

---
+++ blog/src/main/java/cloudclub/blog/global/config/LoginConfig.java
@@ -0,0 +1,19 @@
+package cloudclub.blog.global.config;
+
+import cloudclub.blog.global.LoginUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class LoginConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
